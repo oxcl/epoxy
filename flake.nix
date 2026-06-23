759dea3efe728536901df9b3a4f2fb75b8d7cf1a
oxcl <oxcl@proton.me>
2026-06-23 15:57:40 +0400

    Add Hyprland dependency and wrapper script

--- flake.nix
+++ flake.nix
@@ -4,19 +4,45 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, hyprland }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        hyprland-pkg = hyprland.packages.${system}.hyprland;
       in
       {
-        packages.default = pkgs.symlinkJoin {
+        packages.default = pkgs.stdenv.mkDerivation {
           name = "epoxy";
-          paths = [ ];
+          src = ./.;
+
+          propagatedBuildInputs = [
+            hyprland-pkg
+          ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+
+            cat > $out/bin/epoxy << 'SCRIPT'
+#!/bin/sh
+CONFIG_FILE="$HOME/.config/epoxy/hypr/hyprland.lua"
+
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "Config not found: $CONFIG_FILE"
+  exit 1
+fi
+
+exec Hyprland -c "$CONFIG_FILE"
+SCRIPT
+
+            chmod +x $out/bin/epoxy
+          '';
+
           meta = {
-            description = "Epoxy";
+            description = "Epoxy - Hyprland wrapper";
+            mainProgram = "epoxy";
           };
         };
       });
