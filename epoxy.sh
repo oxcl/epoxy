99e6298049e08a0ee199e17827bd08efa263ed7f
oxcl <oxcl@proton.me>
2026-06-23 20:19:39 +0400

    simplify flake and exposy.sh

--- epoxy.sh
+++ epoxy.sh
@@ -1,10 +1,3 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-CONFIG_FILE="$HOME/.config/epoxy/hypr/hyprland.lua"
-
-if [ ! -f "$CONFIG_FILE" ]; then
-  echo "Config not found: $CONFIG_FILE"
-  exit 1
-fi
-
-exec start-hyprland -- -c "$CONFIG_FILE"
+somewm
--- flake.nix
+++ flake.nix
@@ -3,68 +3,63 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     somewm.url = "github:trip-zip/somewm";
   };
 
-  outputs = { self, nixpkgs, flake-utils, somewm }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.cage = pkgs.cage;
+  outputs = { self, nixpkgs, somewm }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "epoxy";
+        src = ./.;
 
-        packages.default = pkgs.stdenv.mkDerivation {
-          name = "epoxy";
-          src = ./.;
+        nativeBuildInputs = [ pkgs.makeWrapper ];
 
-          nativeBuildInputs = [
-            pkgs.makeWrapper
-          ];
+        buildInputs = [
+          pkgs.hyprland
+          pkgs.foot
+          pkgs.rofi
+          pkgs.pi-coding-agent
+        ];
 
-          buildInputs = [
-            pkgs.hyprland
-            pkgs.foot
-            pkgs.rofi-wayland
-            pkgs.pi-coding-agent
-          ];
+        installPhase = ''
+          mkdir -p $out/bin
+          install -Dm755 epoxy.sh $out/bin/epoxy
+          wrapProgram $out/bin/epoxy \
+            --prefix PATH : ${pkgs.lib.makeBinPath [
+              pkgs.hyprland pkgs.foot pkgs.rofi pkgs.pi-coding-agent
+            ]}
+        '';
 
-          installPhase = ''
-            mkdir -p $out/bin
-            install -Dm755 epoxy.sh $out/bin/epoxy
-
-            # Wrap the script so Hyprland and foot are available in PATH
-            wrapProgram $out/bin/epoxy \
-              --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.hyprland pkgs.foot pkgs.rofi-wayland pkgs.pi-coding-agent ]}
-          '';
-
-          meta = {
-            description = "Epoxy - Hyprland wrapper";
-            mainProgram = "epoxy";
-          };
+        meta = {
+          description = "Epoxy - Hyprland wrapper";
+          mainProgram = "epoxy";
         };
+      };
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [
-            somewm.packages.${system}.somewm
-          ];
+      devShells.${system}.default = pkgs.mkShell {
+        inputsFrom = [
+          somewm.packages.${system}.somewm
+        ];
 
-          packages = [
-            somewm.packages.${system}.somewm
-            pkgs.cage
-            pkgs.foot
-            pkgs.lua5_4
-            pkgs.lua-language-server
-            pkgs.grim
-            pkgs.wl-clipboard
-            pkgs.rofi-wayland
-            pkgs.pi-coding-agent
-          ];
+        packages = [
+          somewm.packages.${system}.somewm
+          pkgs.cage
+          pkgs.foot
+          pkgs.lua5_4
+          pkgs.lua-language-server
+          pkgs.grim
+          pkgs.wl-clipboard
+          pkgs.rofi
+          pkgs.pi-coding-agent
+        ];
 
-          shellHook = ''
-            echo "epoxy dev shell loaded"
-          '';
-        };
-      });
+        shellHook = ''
+          echo "epoxy dev shell loaded"
+        '';
+      };
+    };
 }
